0e1ac2eb55cc833df9c4693f64f346afc6054335
Mitachibana <96460079+mitachibana@users.noreply.github.com>
2024-05-28 22:21:07 +0900

    Add files via upload

--- daily.js
+++ daily.js
@@ -1,27 +1,18 @@
 const { tokenWolf, tokenExchange } = require("./config.json");
 
 async function daily() {
-  const data = {
-    astronomy: (
-      await fetch(
-        `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=today%20nagano&output=json&ip=138.199.21.39`
-      )
-    ).json(),
-    gBannerTime: (
-      await fetch(
-        `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=june+4+2024&output=json&ip=138.199.21.39`
-      )
-    ).json(),
-    hBannerTime: (
-      await fetch(
-        `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=june+18+2024&output=json&ip=138.199.21.39`
-      )
-    ).json(),
-    motivation: (await fetch(`https://zenquotes.io/api/random`)).json(),
-    jpword: (
-      await fetch(`https://random-word.ryanrk.com/api/jp/word/random`)
-    ).json(),
-  };
+  const wolfData = await fetch(
+    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=today%20nagano&output=json&ip=138.199.21.39`
+  );
+  const astronomy = await wolfData.json();
+  const wolfTime1 = await fetch(
+    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=jun+4+2024&output=json&ip=138.199.21.39`
+  );
+  const gBannerTime = await wolfTime1.json();
+  const wolfTime2 = await fetch(
+    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=may+29+2024&output=json&ip=138.199.21.39`
+  );
+  const hBannerTime = await wolfTime2.json();
   const exchange = await fetch(
     `https://v6.exchangerate-api.com/v6/${tokenExchange}/latest/usd`
   );
@@ -38,19 +29,22 @@ async function daily() {
     `https://v6.exchangerate-api.com/v6/${tokenExchange}/latest/eur`
   );
   const eur = await exchange4.json();
-
+  const quote = await fetch("https://zenquotes.io/api/random");
+  const motivation = await quote.json();
+  const word = await fetch("https://random-word.ryanrk.com/api/jp/word/random");
+  const jpword = await word.json();
   return `
   :01H12ZH32NFRESS95RA5NRCG0N: ***GOOD DAY ReVoYo!*** :01H12ZH32NFRESS95RA5NRCG0N:
-    ${data.astronomy?.queryresult?.pods?.[1]?.subpods?.[0]?.plaintext}
-    ${data.astronomy?.queryresult?.pods?.[3]?.subpods?.[1]?.plaintext} | ${data.astronomy?.queryresult?.pods?.[3]?.subpods?.[0]?.plaintext}
-    ${data.astronomy?.queryresult?.pods?.[7]?.subpods?.[0]?.plaintext}
+    ${astronomy?.queryresult?.pods?.[1]?.subpods?.[0]?.plaintext}
+    ${astronomy?.queryresult?.pods?.[3]?.subpods?.[1]?.plaintext} | ${astronomy?.queryresult?.pods?.[3]?.subpods?.[0]?.plaintext}
+    ${astronomy?.queryresult?.pods?.[7]?.subpods?.[0]?.plaintext}
 
     :01HM7RWMYHXZYVG6NKQCBVDYYK: **Banners**
-    Current Genshin Banner: Ends on ${data.gBannerTime?.queryresult?.pods?.[0]?.subpods?.[0]?.plaintext}
-    Time left: ${data.gBannerTime?.queryresult?.pods?.[2]?.subpods?.[0]?.plaintext}
+    Current Genshin Banner: Ends on ${gBannerTime?.queryresult?.pods?.[0]?.subpods?.[0]?.plaintext}
+    Time left: ${gBannerTime?.queryresult?.pods?.[2]?.subpods?.[0]?.plaintext}
 
-    Current HSR Banner: Ends on ${data.hBannerTime?.queryresult?.pods?.[0]?.subpods?.[0]?.plaintext}
-    Time left: ${data.hBannerTime?.queryresult?.pods?.[2]?.subpods?.[0]?.plaintext}
+    Current HSR Banner: Ends on ${hBannerTime?.queryresult?.pods?.[0]?.subpods?.[0]?.plaintext}
+    Time left: ${hBannerTime?.queryresult?.pods?.[2]?.subpods?.[0]?.plaintext}
 
     :01HM7S2WR7G8W1N4QP0RR9K2JC: **Exchange Rates**
     *For those looking to import goods*
@@ -60,16 +54,16 @@ async function daily() {
     EUR/JPY: ${eur.conversion_rates.JPY}
 
     :01HD3VTA1Y9SYSR1YD5JJWSEBZ: **Moon Phase:**
-    ${data.astronomy?.queryresult?.pods?.[6]?.subpods?.[0]?.plaintext}
+    ${astronomy?.queryresult?.pods?.[6]?.subpods?.[0]?.plaintext}
 
     **Quote of the Day:**
-    *${data.motivation?.[0]?.q} 
-    -${data.motivation?.[0]?.a}*
+    *${motivation?.[0]?.q} 
+    -${motivation?.[0]?.a}*
 
     **Japanese Word of the Day:**
-    ${data.jpword?.[0]}
+    ${jpword?.[0]}
 
-    *Pinging <@01H0M62PT3AXCQY4V0CAP08CDC> to notify of boot, Misaka explains with increasing irritation.*
+    *Pinging <@01H0M62PT3AXCQY4V0CAP08CDC> to notify of function execution, Misaka explains with increasing irritation.*
     `;
 }
 module.exports.daily = daily;
