3df09d9dbd61dd267abf3e3846d1c9eed2824f19
Mitachibana <96460079+mitachibana@users.noreply.github.com>
2024-12-30 13:01:18 +0000

    Update daily.js

--- daily.js
+++ daily.js
@@ -6,7 +6,7 @@ async function daily() {
   );
   const astronomy = await wolfData.json();
   const wolfTime1 = await fetch(
-    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=december+31+2024&output=json&ip=138.199.21.39`
+    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=january+21+2024&output=json&ip=138.199.21.39`
   );
   const gBannerTime = await wolfTime1.json();
   const wolfTime2 = await fetch(
