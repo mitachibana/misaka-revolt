d562aa65de74f5c571c1d1ea922ede6dd6db0e81
Mitachibana <96460079+mitachibana@users.noreply.github.com>
2025-04-17 13:07:10 +0900

    Update index.js
    reset time to 23:59 (Germany/Netherlands)

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.on("logout", () =>
 
 async function greeting() {
   var date = new Date();
-  if (date.getHours() === 6 && date.getMinutes() === 05) {
+  if (date.getHours() === 23 && date.getMinutes() === 59) {
     client.channels
       .get("01HM6DKFX0W1D1MYR06RD1D69A")
       .sendMessage(`${await daily()}`);
