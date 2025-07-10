405b6e880fdd1ba7cdef2e6d246f078cb80271b6
Mitachibana <96460079+mitachibana@users.noreply.github.com>
2025-07-10 10:14:09 +0900

    Update daily.js

--- daily.js
+++ daily.js
@@ -2,7 +2,7 @@ const { tokenWolf, tokenExchange } = require("./config.json");
 
 async function daily() {
   const wolfTime1 = await fetch(
-    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=july+8+2025&output=json`
+    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=july+29+2025&output=json`
   );
   const gBannerTime = await wolfTime1.json();
   const wolfTime2 = await fetch(
@@ -37,7 +37,7 @@ async function daily() {
   :01H12ZH32NFRESS95RA5NRCG0N: ***GOOD DAY ReVoYo!*** :01H12ZH32NFRESS95RA5NRCG0N:
 
     :salt: **Banners**
-    ***Genshin Banner | 5.7 Phase 1: Skirk DEBUT, Shenhe, Dahlia, Candace, Diona***
+    ***Genshin Banner | 5.7 Phase 2: Mauvika, Emilie, Xiangling, Iansan, Yaoyao***
     Ends on ${gBannerTime?.queryresult?.pods?.[0]?.subpods?.[0]?.plaintext}
     Time left: ${gBannerTime?.queryresult?.pods?.[2]?.subpods?.[0]?.plaintext}
     
