2826f7dfc88df7873eb9979fc29ec79dd08fca30
Mitachibana <96460079+mitachibana@users.noreply.github.com>
2025-09-03 20:19:25 +0900

    Update miyabi.js

--- miyabi.js
+++ miyabi.js
@@ -2,14 +2,16 @@ const { tokenWolf, tokenExchange } = require("./config.json");
 
 async function miyabi() {
   const wolfData = await fetch(
-    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=august+6+2025%20nagano&output=json&ip=138.199.21.39`
+    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=september+3+2025%20nagano&output=json&ip=138.199.21.39`
   );
   const hoshimi = await wolfData.json();
   return `
   ## Daily Miyabi Adoration Update
-  > **Miyabi Banner End Date (6 August 2025):** ${hoshimi?.queryresult?.pods?.[2]?.subpods?.[0]?.plaintext}
+  > **Alice Banner End Date (3 September 2025):** ${hoshimi?.queryresult?.pods?.[2]?.subpods?.[0]?.plaintext}
   
-  THE HOSHIMI MIYABI RERUN IS HERE AT LAST! MAY ALL MIYABI WANTERS BE MIYABI HAVERS. PRAISE BE!
+  Counting the days since Alice's debut and counting down the days until her rerun!
+  
+  Post wipeout pictures and other art of Alice!
 
   ***Your post must contain only the source and picture. No messages and no NSFW! Reactions OK!***
 
@@ -17,3 +19,4 @@ async function miyabi() {
     `;
 }
 module.exports.miyabi = miyabi;
+
