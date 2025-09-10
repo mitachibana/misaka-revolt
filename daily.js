3104356702cbb47c700cc487ff36b675339a2639
Mitachibana <96460079+mitachibana@users.noreply.github.com>
2025-09-10 23:56:31 +0900

    daily.js の更新

--- daily.js
+++ daily.js
@@ -2,7 +2,7 @@ const { tokenWolf, tokenExchange } = require("./config.json");
 
 async function daily() {
   const wolfTime1 = await fetch(
-    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=september+9+2025&output=json`
+    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=september+30+2025&output=json`
   );
   const gBannerTime = await wolfTime1.json();
   const wolfTime2 = await fetch(
@@ -37,7 +37,7 @@ async function daily() {
   :01H12ZH32NFRESS95RA5NRCG0N: ***GOOD DAY ReVoYo!*** :01H12ZH32NFRESS95RA5NRCG0N:
 
     :salt: **Banners**
-    ***Genshin Banner | 5.8 Phase 2: Mualani, Chasca, Bennett, Ororon, Ifa***
+    ***Genshin Banner | 6.0 Luna 1: Lauma DEBUT, Nahida, Barbara, Kuki, Kaveh***
     Ends on ${gBannerTime?.queryresult?.pods?.[0]?.subpods?.[0]?.plaintext}
     Time left: ${gBannerTime?.queryresult?.pods?.[2]?.subpods?.[0]?.plaintext}
     
