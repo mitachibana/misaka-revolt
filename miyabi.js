e36753f9bb8b539ad46aa2aab22820fa5364e4a5
Mitachibana <96460079+mitachibana@users.noreply.github.com>
2025-12-06 22:02:26 +0900

    Update miyabi.js

--- miyabi.js
+++ miyabi.js
@@ -2,21 +2,28 @@ const { tokenWolf, tokenExchange } = require("./config.json");
 
 async function miyabi() {
   const wolfData = await fetch(
-    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=september+3+2025%20nagano&output=json&ip=138.199.21.39`
+    `http://api.wolframalpha.com/v2/query?appid=${tokenWolf}&input=december+30+2025%20nagano&output=json&ip=138.199.21.39`
   );
   const hoshimi = await wolfData.json();
   return `
-  ## Daily Alice Wipeouts!
-  > **Last Alice Banner End Date (3 September 2025):** ${hoshimi?.queryresult?.pods?.[3]?.subpods?.[0]?.plaintext}
-  
-  Counting the days since Alice's debut and counting down the days until her rerun!
-  
-  Post wipeout pictures and other art of Alice!
+  ## ReVoYo Reminders!
+  We hope everyone is having a good day! We wanted to remind everyone about how to reach out to a mod if you need anything.
+  The easiest way is to just DM or ping a mod:
+  - mitachibana#1667
+  - suichanwaa#4961
+  - Thijnmens#4468
+  ### In case of emergency:
+  You can contact us via the Google Form below. All admins get a notification whenever someone submits a response. This is the failsafe in case the Stoat platform becomes unuseable for any reason.
+  (No email addresses or personal information is collected by the form. You may choose enter your username if you wish to be contacted, however)
+  https://forms.gle/i9vNpx4w2yGosfu87
 
-  ***Your post must contain only the source and picture. No messages and no NSFW! Reactions OK!***
-
-    Pinging <@01H0M62PT3AXCQY4V0CAP08CDC> to remind the monke he must post a picture, Misaka explains with futility.
+  ## YE SHUNGUANG WORSHIP
+  
+  > **Ye Shunguang Debut Banner Start Date (30 December 2025):** ${hoshimi?.queryresult?.pods?.[3]?.subpods?.[0]?.plaintext}
+  
+  #### Post art and messages about Ye Shunguang in <#01KBSVRTNXJ443RD169WNKV3Y6>! We know you all absolutely adore her, but please remember to follow the rules!
     `;
 }
 module.exports.miyabi = miyabi;
+
 
