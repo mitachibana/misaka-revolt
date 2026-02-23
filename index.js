1d54f47fec3bdc33924033227984661d413e8723
Mitachibana <96460079+mitachibana@users.noreply.github.com>
2026-02-23 23:42:48 +0900

    index.js の更新

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.on("logout", () =>
 
 async function greeting() {
   var date = new Date();
-  if (date.getHours() === 7 && date.getMinutes() === 50) {
+  if (date.getHours() === 22 && date.getMinutes() === 10) {
     client.channels
       .get(channelGreeting)
       .sendMessage(`${await daily()}`);
